6136d0abbaca65f4123dc75077f42df5fa797021
leeyulgok <dbfrhr20@gmail.com>
2023-11-05 15:21:53 +0900

    4장: init

---
+++ __test__/Day4/Application.test.js
@@ -0,0 +1,6 @@
+import App from "../../src/Day4/App";
+describe('App 테스트', () => {
+  test('임시 테스트', () => {
+    expect().toBe();
+  });
+});
---
+++ src/Day4/App.js
@@ -0,0 +1,5 @@
+class App {
+  async start() {}
+};
+
+export default App;
